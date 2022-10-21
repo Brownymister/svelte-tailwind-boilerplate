829907e310293965e16b200ade096c028ab86f3e
Browny <julian.kramer.berlin@icloud.com>
2022-10-21 15:14:49 +0200

    init new vite project

---
+++ README.md
@@ -0,0 +1,2 @@
+# svelte-tailwind-boilerplate
+
---
+++ src/App.svelte
@@ -0,0 +1,23 @@
+<script>
+  import svelteLogo from './assets/svelte.svg'
+</script>
+
+<main>
+  <div>
+    <a href="https://tailwindcss.com"> 
+      <img src="/tailwind.svg" class="logo" alt="Tailwind Logo" />
+    </a>
+    <a href="https://svelte.dev"> 
+      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
+    </a>
+  </div>
+  <h1>Tailwind + Svelte</h1>
+
+</main>
+
+<style>
+    .logo {
+        width: 3rem;
+
+    }
+</style>
