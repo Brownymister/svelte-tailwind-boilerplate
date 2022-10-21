44fad77e9e74beadb0ad5057a2e502466ba16598
Browny <julian.kramer.berlin@icloud.com>
2022-10-21 20:54:50 +0200

    add documentation

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
-# svelte-tailwind-boilerplate
-
+<h1 style="text-align:center;">💨 svelte-tailwind-boilerplate </h1>
 Biolerplate project for using tailwind within svelte.
+
+## Quick start 
+
+1. Make sure that you have node and npm installed.
+2. Cloe this Repo like this: `git clone --depth=1 https://github.com/Brownymister/svelte-tailwind-boilerplate.git <YOUR_PROJECT_NAME>`
+3. Change into the project directory: `cd <YOUR_PROJECT_NAME>`
+4. Run `npm install` for installing Dependencies 
+5. Run `npm run dev` for starting the dev server.
--- src/App.svelte
+++ src/App.svelte
@@ -3,14 +3,17 @@
 </script>
 
 <main>
-  <div class="m-16 flex flex-row items-center justify-center">
+    <div class="m-16 flex flex-row items-center justify-center">
     <a href="https://tailwindcss.com"> 
         <img class="w-20 m-8" src="/tailwind.svg" alt="Tailwind Logo" />
     </a>
     <a href="https://svelte.dev"> 
         <img class="w-20 m-4" src={svelteLogo} alt="Svelte Logo" />
     </a>
-  </div>
-  <h1 class="font-bold text-5xl text-gray-400 text-center">Tailwind + Svelte</h1>
+
+    </div>
+    <h1 class="font-bold text-5xl text-gray-400 text-center">Tailwind + Svelte</h1>
+
+    <p class="text-gray-400 text-lg text-center m-16">Try edit <span class="rounded-md bg-gray-800 p-1">./src/App.svelte</span></p>
 
 </main>
